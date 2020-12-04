37a05be1d5efd50439ad0118f50f16a13e4c46da
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-12-04 20:55:31 +0100

    trying to fix things

--- src/units/JollyUnit.py
+++ src/units/JollyUnit.py
@@ -7,23 +7,24 @@ from .UnitBase import UnitBase
 
 class JollyUnit(UnitBase):
 
-	action: int = 0 # 0 = normal, 1 = wrong, 2 = error, 3 = fill, 4 = multi, 5 = malus
+	action: int = 0  # 0 = normal, 1 = wrong, 2 = error, 3 = fill, 4 = multi, 5 = malus
 	changeActionTime: float
 
-	def __init__(self, pos: wx.Point, speed: int):
+	def __init__(self, pos: wx.Point, speed: int, initialState=0):
 		super( JollyUnit, self ).__init__(
 			pos=pos,
 			speed=speed,
 			color=wx.Colour('#0390FC')
 		)
 		self.changeActionTime = time()
+		self.action = initialState
 
 	def OnTick( self, evt ):
-		if time() - self.changeActionTime > 5:
+		if time() - self.changeActionTime > 30:
 			if self.action == 5:
 				self.action = 0
 			else:
-				self.action	+= 1
+				self.action += 1
 			if self.action == 0:  # normal
 				self.SetBackgroundColour( wx.Colour('#0390FC') )
 			elif self.action == 1:  # wrong
@@ -36,15 +37,17 @@ class JollyUnit(UnitBase):
 				self.SetBackgroundColour( wx.Colour('#2FF5F1') )    # TODO: missing multi and malus, using placeholder colors
 			else:  # malus
 				self.SetBackgroundColour( wx.Colour('#2FF5F1') )
+			self.Refresh()
 		super(JollyUnit, self).OnTick(evt)
 
 	def OnBarTouch( self ):
 		if self.action == 0:
-			self.bar.score += 2
+			self.bar.score += 5
 		elif self.action == 1:
 			self.bar.score -= 5
 		elif self.action == 2:
 			self.bar.EndGame()
+			return
 		elif self.action == 3:
 			self.bar.score += 100
 		elif self.action == 4:
