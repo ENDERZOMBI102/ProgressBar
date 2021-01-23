dc2c0517a18bcc87d65bf130d1bbb52fecc5b208
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-01-23 21:11:40 +0100

    now subclasses Tickable and Collidable
    removed rendundant pos property
    removed title
    
    cleared OnTick method
    added OnCollide method
    
    Signed-off-by: ENDERZOMBI102 <enderzombi102.end@gmail.com>

--- src/windows/WindowBase.py
+++ src/windows/WindowBase.py
@@ -1,24 +1,23 @@
 import wx
 
+from BaseClasses import Collidable, Tickable
 
-class WindowBase(wx.Frame):
 
-	pos: wx.Point
+class WindowBase(wx.Frame, Tickable, Collidable):
+
 	size: wx.Size = wx.Size(100, 200)
 	dc: wx.WindowDC
 
-	def __init__(self, pos: wx.Point, title: str = 'Distraction'):
+	def __init__(self, pos: wx.Point):
 		super(WindowBase, self).__init__(
 			parent=wx.GetApp().gameStage,
 			pos=pos,
 			size=self.size,
-			title=title,
 			style=wx.BORDER_NONE | wx.FRAME_NO_TASKBAR
 		)
-		self.pos = pos
 		self.dc = wx.WindowDC(self)
-		self.dc.SetPen( wx.Pen( wx.GetApp().GetColor('WHITE') ) )
-		self.dc.SetBrush( wx.Brush( wx.GetApp().GetColor('WHITE') ) )
+		self.dc.SetPen( wx.Pen( wx.GetApp().GetColor('GREEN') ) )
+		self.dc.SetBrush( wx.Brush( wx.GetApp().GetColor('GREEN') ) )
 		self.dc.DrawRectangle(0, 0, 100, 100)
 
 		self.Show()
@@ -28,6 +27,8 @@ class WindowBase(wx.Frame):
 		wx.GetApp().windows.remove(self)
 		self.Destroy()
 
-	def OnTick( self ):
-		self.SetPosition(self.pos)
-		self.SetSize(self.size)
+	def OnTick( self ) -> None:
+		pass
+
+	def OnCollide( self, bbox: wx.Rect ) -> None:
+		pass
