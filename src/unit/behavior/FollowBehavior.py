389c6527a66c2d930e372c8bac6a36ce5a3b3ac6
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-01-24 17:16:35 +0100

    added FollowBehavior
    Signed-off-by: ENDERZOMBI102 <enderzombi102.end@gmail.com>

--- src/unit/behavior/FollowBehavior.py
+++ src/unit/behavior/FollowBehavior.py
@@ -7,20 +7,21 @@ from unit.behavior.BehaviorBase import BehaviorBase
 
 class FollowBehavior(BehaviorBase):
 
-	followTimer: float = 0
+	followTimer: float
 
 	def __init__( self, unit: UnitBase ):
 		super().__init__(unit)
+		self.followTimer = time()
 
 	def Think( self, player: LoadingBar, stage: 'GameStage'):
 		if time() - self.moveTimer > self.unit.speed * 0.0020:
-			self.unit.bbox.Offset( 0, 10 )
-			self.moveTimer = time()
+			offset: int = 0
 			# follow player X?
-			if time() - self.followTimer > 0.12:
-				if self.unit.bbox.GetLeft() < player.GetRect().GetLeft():
-					offset = -10
-				else:
+			if time() - self.followTimer > 0.20:
+				if self.unit.bbox.GetRight() < player.GetRect().GetRight():
 					offset = 10
-				self.unit.bbox.Offset( offset, 0 )
+				else:
+					offset = -10
 				self.followTimer = time()
+			self.unit.bbox.Offset( offset, 10 )
+			self.moveTimer = time()
